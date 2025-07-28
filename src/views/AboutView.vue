572f8e78c501716637af789d088d1bf26b62a4da
Maxwell Ward <max.jp.ward@gmail.com>
2025-07-28 13:42:42 -0700

    create project structure

---
+++ src/views/AboutView.vue
@@ -0,0 +1,6 @@
+<template>
+	<div>
+		<p>The Taquito Test dApp will demonstrate Tezos blockchain interactions through clear narratives, visual
+			diagrams, and hands-on examples.</p>
+	</div>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,5 @@
+<template>
+	<div>
+		<p>Taquito Test dApp</p>
+	</div>
+</template>
