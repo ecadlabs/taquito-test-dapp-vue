7dbc8f8258e08aaafdd90854316c4e9efeecb85a
Maxwell Ward <max.jp.ward@gmail.com>
2025-07-29 14:12:06 -0700

    add header navigation

--- src/components/header-component.vue
+++ src/components/header-component.vue
@@ -1,12 +1,43 @@
 <template>
-  <div class="bg-gray-500 flex justify-between items-center p-3 text-white">
-    <RouterLink :to="{ name: 'home' }">
-      Taquito Playground
+  <div class="flex justify-between items-center p-3 mx-4">
+    <RouterLink :to="{ name: 'home' }" class="font-medium leading-none flex gap-1.5 items-center p-2">
+      <img src="@/assets/logo.svg" alt="Taquito Logo" class="size-5">
+      <p>Taquito Playground</p>
     </RouterLink>
-    <WalletControl />
+
+    <NavigationMenu class="[&_div.absolute]:left-auto [&_div.absolute]:right-0">
+      <NavigationMenuList>
+        <NavigationMenuItem>
+          <RouterLink :to="{ name: 'tests' }">
+            <NavigationMenuLink :class="navigationMenuTriggerStyle()">
+              Examples
+            </NavigationMenuLink>
+          </RouterLink>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <NavigationMenuLink href="https://taquito.io/docs/quick_start" :class="navigationMenuTriggerStyle()">
+            Taquito Documentation
+          </NavigationMenuLink>
+        </NavigationMenuItem>
+      </NavigationMenuList>
+    </NavigationMenu>
+
+    <!-- <WalletControl /> -->
   </div>
+
+
 </template>
 
 <script setup lang="ts">
-import WalletControl from '@/components/wallet-control.vue';
+// import WalletControl from '@/components/wallet-control.vue';
+
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  navigationMenuTriggerStyle,
+} from '@/components/ui/navigation-menu'
+
+
 </script>
