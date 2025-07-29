3dd5135748b8c9339767eb2c193008e2c06a6baf
Maxwell Ward <max.jp.ward@gmail.com>
2025-07-29 12:56:19 -0700

    wait for confirmation before updating wallet balance

--- src/modules/tests/tests/transfer/transfer-tez.ts
+++ src/modules/tests/tests/transfer/transfer-tez.ts
@@ -5,7 +5,12 @@ const send = async (to: string, amount: number) => {
 	const Tezos = walletStore.getTezos;
 
 	try {
-		await Tezos.wallet.transfer({ to, amount }).send();
+		const transfer = await Tezos.wallet.transfer({ to, amount }).send();
+		// May want to make the number of confirmations configurable
+		const confirmationResponse = await transfer.confirmation();
+		console.log(confirmationResponse);
+
+		await walletStore.fetchBalance();
 	} catch (error) {
 		console.error(`Failed to send transfer to '${to}': ${error}`);
 		throw error;
