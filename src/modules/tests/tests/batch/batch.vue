6d73dedaa68a28b65b44010b5168da4378337ddd
Maxwell Ward <max.jp.ward@gmail.com>
2025-08-19 14:19:09 -0700

    add loading state to send batch operation button

--- src/modules/tests/tests/batch/batch.vue
+++ src/modules/tests/tests/batch/batch.vue
@@ -4,8 +4,9 @@
       Batch operation will send a transfer of 1 tez to yourself, and increment
       the counter contract storage by 1.
     </p>
-    <Button @click="sendBatchOperation()">
-      <Group class="size-6" />
+    <Button @click="sendBatchOperation()" :disabled="sending">
+      <Group v-if="!sending" class="size-6" />
+      <LoaderCircle v-else class="size-5 animate-spin" />
       <p>Send Batch Operation</p>
     </Button>
   </div>
@@ -14,8 +15,8 @@
 <script setup lang="ts">
 import { useDiagramStore } from "@/stores/diagramStore";
 import Button from "@/components/ui/button/Button.vue";
-import { onMounted } from "vue";
-import { Group } from "lucide-vue-next";
+import { onMounted, ref } from "vue";
+import { Group, LoaderCircle } from "lucide-vue-next";
 import { sendBatch } from "@/modules/tests/tests/batch/batch";
 
 const diagramStore = useDiagramStore();
@@ -24,7 +25,10 @@ onMounted(() => {
   diagramStore.setTestDiagram("batch");
 });
 
+const sending = ref(false);
 const sendBatchOperation = async () => {
+  sending.value = true;
   await sendBatch();
+  sending.value = false;
 };
 </script>
