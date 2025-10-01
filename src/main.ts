ab08800a0f1fc7bb5714f1a4e38ecb9146e9a864
Maxwell Ward <max.jp.ward@gmail.com>
2025-09-30 19:27:42 -0700

    implement sentry

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,16 @@ import "@/style.css";
 import App from "@/App.vue";
 import router from "@/router";
 import { createPinia } from "pinia";
+import * as Sentry from "@sentry/vue";
 
 const pinia = createPinia();
 const app = createApp(App);
+
+Sentry.init({
+  app,
+  dsn: import.meta.env.VITE_SENTRY_DSN,
+  sendDefaultPii: false,
+});
 
 app.use(router);
 app.use(pinia);
