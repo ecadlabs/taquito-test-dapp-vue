c8f10688d462b7cfdb4d9eb5569d007cc78e29f6
Maxwell Ward <max.jp.ward@gmail.com>
2025-10-20 10:05:32 -0700

    various colour fixes for dark mode

--- src/components/test-diagram.vue
+++ src/components/test-diagram.vue
@@ -658,8 +658,9 @@ function isNodeCompleted(node: TypedNode | undefined): boolean {
 }
 
 .dark .caution-path .connection-line.horizontal {
-  background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' %3E%3Cline x1='0' y1='50%25' x2='100%25' y2='50%25' stroke='oklch(0.209 0 0)' stroke-width='3' stroke-dasharray='5 5'/%3E%3C/svg%3E");
+  background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' %3E%3Cline x1='0' y1='50%25' x2='100%25' y2='50%25' stroke='%23f97316' stroke-width='3' stroke-dasharray='5 5'/%3E%3C/svg%3E");
   background-repeat: repeat-x;
+  background-color: var(--background);
 }
 
 .dark .neutral-path .connection-line.horizontal {
